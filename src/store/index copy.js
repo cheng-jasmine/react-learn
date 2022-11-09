dc520d6c5b578aa8d0cc0a719cba5d37a1be7175
chengli <224436641@qq.com>
2022-11-09 16:39:51 +0800

    applyMiddleware

--- src/store/index copy.js
+++ src/store/index copy.js
@@ -1,4 +1,4 @@
-import { createStore, bindActionCreators } from "redux"
+import { createStore, bindActionCreators } from "../redux"
 
 import reducer from "./reducer"
 import { v4 as uuidv4 } from 'uuid';
@@ -8,9 +8,9 @@ import { createAddUserAction, createDeleteserAction } from "./action/usersAction
 const store = createStore(reducer)
 
 
-const unListen = store.subscribe(() => {
-    console.log("监听器1：", store.getState())
-})
+// const unListen = store.subscribe(() => {
+//     console.log("监听器1：", store.getState())
+// })
 // store.dispatch(createAddUserAction({
 //     id: 3,
 //     name: "cheng",
@@ -19,6 +19,14 @@ const unListen = store.subscribe(() => {
 // store.dispatch(createDeleteserAction(3))
 // unListen() // 取消监听
 
+const oldDispatch = store.dispatch
+store.dispatch = function (action) {
+    console.log("旧数据:", store.getState())
+    console.log("action:", action)
+    oldDispatch(action)
+    console.log("新数据:", store.getState())
+    console.log("")
+}
 
 
 
